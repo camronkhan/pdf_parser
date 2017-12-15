769c9d214375d0579165f636cdd897354baed2a1
Camron Khan <camron.khan@zurichna.com>
2017-12-15 08:45:58 -0600

    successfully uploading

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const express = require('express')();
+const express = require('express');
 const multer = require('multer');
-const pdf2json = require('pdf2json');
+const path = require('path');
+// const pdf2json = require('pdf2json');
 
-const port = 3000;
-
-express.get('/', (req, res) => {
-  res.send('Hello world');
+// Set storage engine
+const storage = multer.diskStorage({
+    destination: './tmp/',
+    filename: (req, file, cb) => cb(null, `${Date.now()}_${file.originalname}`)
 });
 
-express.listen(port, () => console.log(`Server running on port ${port}`));
+// Only allow single file upload and file key in POST body must be 'pdf'
+const upload = multer({ storage: storage }).single('pdf');
+
+// Initialize app
+const app = express();
+
+// GET
+app.get('/', (req, res) => res.send('Hello world'));
+
+// POST
+app.post('/upload', (req, res) =>
+    upload(req, res, (err) =>
+        err ? res.send(err) : res.send(req.file)));
+
+// Listen on port 3000
+const port = 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
